91715b82ceb3710d835a370fe95ce4f510dc3ac2
tt <mounttain@gmail.com>
2020-02-06 19:53:53 +0100

    fixed the FAQ page and updated the top navbar based on globals.currentUser

--- app/templates/faq/faq.controller.js
+++ app/templates/faq/faq.controller.js
@@ -7,35 +7,7 @@
 
     FaqController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
     function FaqController($location, AuthenticationService, FlashService) {
-        var vm = this;
-
-        vm.isActive = function (viewLocation) {
-          return viewLocation === $location.path();
-        };
-
-        // search
-        vm.searchInput = "";
-        vm.submitSearch = function(searchObject) {
-          $location.path("/search");
-        };
-
-        (function initController() {
-            // reset login status
-            // AuthenticationService.ClearCredentials();
-        })();
-
-        // function login() {
-        //     vm.dataLoading = true;
-        //     AuthenticationService.Login(vm.username, vm.password, function (response) {
-        //         if (response.success) {
-        //             AuthenticationService.SetCredentials(vm.username, vm.password);
-        //             $location.path('/');
-        //         } else {
-        //             FlashService.Error(response.message);
-        //             vm.dataLoading = false;
-        //         }
-        //     });
-        // };
-    }
+      var vm = this;
+  }
 
 })();
